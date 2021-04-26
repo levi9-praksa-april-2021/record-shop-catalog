3e5cfb7057f726fb5be28266ac6d117ed0a4479b
Igor <kuja.qja96@gmail.com>
2021-04-26 13:07:33 +0200

    Fixed create genre

--- src/main/java/com/recordshop/catalog/web/artist/CreateArtistRequest.java
+++ src/main/java/com/recordshop/catalog/web/artist/CreateArtistRequest.java
@@ -3,13 +3,15 @@ package com.recordshop.catalog.web.artist;
 import javax.validation.constraints.NotBlank;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class CreateArtistRequest {
 	
 	@NotBlank(message = "First name cannot be blank")
-	private final String firstName;
+	private String firstName;
 	@NotBlank(message = "Last name cannot be blank")
-	private final String lastName;
+	private String lastName;
     
 }
--- src/main/java/com/recordshop/catalog/web/genre/CreateGenreRequest.java
+++ src/main/java/com/recordshop/catalog/web/genre/CreateGenreRequest.java
@@ -3,10 +3,12 @@ package com.recordshop.catalog.web.genre;
 import javax.validation.constraints.NotBlank;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class CreateGenreRequest {
 	
 	@NotBlank(message = "Name cannot be blank")
-	private final String name;
+	private String name;
 }
