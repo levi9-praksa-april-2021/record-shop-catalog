9060529a9dfc21cd7ac02a75612ef36b21bbe445
Matija Petrovic <matijapetrovicweb@gmail.com>
2021-05-05 13:53:47 +0200

    Added no-arg constructor to requests

--- src/main/java/com/recordshop/catalog/web/record/CreateRecordRequest.java
+++ src/main/java/com/recordshop/catalog/web/record/CreateRecordRequest.java
@@ -1,25 +1,27 @@
 package com.recordshop.catalog.web.record;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.*;
 import java.math.BigDecimal;
 import java.util.List;
 
 @Data
+@NoArgsConstructor
 public class CreateRecordRequest {
     @NotBlank(message = "Title cannot be blank")
-    private final String title;
+    private String title;
     @NotBlank(message = "Album cannot be blank")
-    private final String album;
+    private String album;
     @PositiveOrZero(message = "Price cannot be negative")
-    private final BigDecimal price;
+    private BigDecimal price;
     @PositiveOrZero(message = "Stock cannot be negative")
-    private final Integer stock;
+    private Integer stock;
 
     @NotEmpty(message = "Record must have at least one artist")
-    private final List<Long> artistIds;
+    private List<Long> artistIds;
 
     @NotEmpty(message = "Record must have at least one genre")
-    private final List<Long> genreIds;
+    private List<Long> genreIds;
 }
--- src/main/java/com/recordshop/catalog/web/record/UpdateRecordRequest.java
+++ src/main/java/com/recordshop/catalog/web/record/UpdateRecordRequest.java
@@ -1,6 +1,7 @@
 package com.recordshop.catalog.web.record;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
@@ -9,17 +10,18 @@ import java.math.BigDecimal;
 import java.util.List;
 
 @Data
+@NoArgsConstructor
 public class UpdateRecordRequest {
     @NotBlank(message = "Title cannot be blank")
-    private final String title;
+    private String title;
     @NotBlank(message = "Album cannot be blank")
-    private final String album;
+    private String album;
     @PositiveOrZero(message = "Price cannot be negative")
-    private final BigDecimal price;
+    private BigDecimal price;
 
     @NotEmpty(message = "Record must have at least one artist")
-    private final List<Long> artistIds;
+    private List<Long> artistIds;
 
     @NotEmpty(message = "Record must have at least one genre")
-    private final List<Long> genreIds;
+    private List<Long> genreIds;
 }
